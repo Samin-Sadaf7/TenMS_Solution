ec2b8e51b2b2e43e69a961cc5d0c48e8345be01e
Samin-Sadaf7 <saminsadaf@iut-dhaka.edu>
2025-07-25 21:50:35 +0600

    Feature: Set up LLM

---
+++ app/llm_client.py
@@ -0,0 +1,11 @@
+from langchain.chat_models import ChatCohere 
+
+import os
+# Load from environment
+COHERE_API_KEY = os.getenv("cohere_api_key")
+
+llm = ChatCohere(
+    cohere_api_key=COHERE_API_KEY, 
+    model="command-r", 
+    streaming=True
+) 
