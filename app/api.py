271e7c532b0069df13d39a03769341d2a218b6a7
Samin-Sadaf7 <saminsadaf@iut-dhaka.edu>
2025-07-25 21:54:02 +0600

    Feature: Completed Endpoints

--- app/api.py
+++ app/api.py
@@ -1,4 +1,22 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Query
+from app.models import QueryRequest
+from app.core import rag_chat_with_memory
+from app.memory import memory
+from app.retrieval import retrieve_with_scores
 
 router = APIRouter()
+
+@router.post("/query")
+def answer_query(request: QueryRequest):
+    answer = rag_chat_with_memory(request.query)
+    return {"query": request.query, "answer": answer}
+
+@router.get("/last-messages")
+def get_last_two_messages():
+    messages = memory.chat_memory.messages[-4:]
+    return [{"role": msg.type, "content": msg.content} for msg in messages]
+
+@router.get("/relevant-contexts")
+def get_contexts(query: str = Query(...)):
+    results = retrieve_with_scores(query)
+    return results
