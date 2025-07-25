c84e9daf6c6a7b7594f71d994f7d6c9a3c1cbb61
Samin-Sadaf7 <saminsadaf@iut-dhaka.edu>
2025-07-26 00:14:40 +0600

    Feature: Tested with other models

--- app/vectorstore.py
+++ app/vectorstore.py
@@ -1,8 +1,14 @@
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
+import torch
 
-
-device = "cpu"
+# Check device
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"  # Apple Silicon GPU acceleration device
+else:
+    device = "cpu"
 
 embedding_model = HuggingFaceEmbeddings(
     model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
