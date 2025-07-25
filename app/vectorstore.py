509a91be1adafbb214b61e2ff5bb5ff75c86f4df
Samin-Sadaf7 <saminsadaf@iut-dhaka.edu>
2025-07-25 22:57:07 +0600

    Refactor: VectorStore.py to only run on cpu

--- app/vectorstore.py
+++ app/vectorstore.py
@@ -1,14 +1,8 @@
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
-import torch
 
-# Check device
-if torch.cuda.is_available():
-    device = "cuda"
-elif torch.backends.mps.is_available():
-    device = "mps"  # Apple Silicon GPU acceleration device
-else:
-    device = "cpu"
+
+device = "cpu"
 
 embedding_model = HuggingFaceEmbeddings(
     model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
