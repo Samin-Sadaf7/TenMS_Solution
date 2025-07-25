e4cbc37e7ad3ece8456ef67dc4e6bbff4d869254
Samin-Sadaf7 <saminsadaf@iut-dhaka.edu>
2025-07-25 22:08:13 +0600

    Refactor: Updated for using mps

--- app/vectorstore.py
+++ app/vectorstore.py
@@ -1,6 +1,18 @@
 from langchain.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
+import torch
 
-# Load FAISS index
-embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+# Check device
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"  # Apple Silicon GPU acceleration device
+else:
+    device = "cpu"
+
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
+    model_kwargs={"device": device}
+)
+
 faiss_store = FAISS.load_local("faiss_index", embedding_model, allow_dangerous_deserialization=True)
