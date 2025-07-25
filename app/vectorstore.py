bdd0dd8371f15301dea0b95c3eb7ea412cfe042e
Samin-Sadaf7 <saminsadaf@iut-dhaka.edu>
2025-07-25 21:39:46 +0600

    Feature: Added vectorstore

---
+++ app/vectorstore.py
@@ -0,0 +1,6 @@
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+
+# Load FAISS index
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+faiss_store = FAISS.load_local("faiss_index", embedding_model, allow_dangerous_deserialization=True)
