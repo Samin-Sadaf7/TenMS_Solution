7a5b7f240730f486f9cfc3ee5f90d8123aee38fd
Samin-Sadaf7 <saminsadaf@iut-dhaka.edu>
2025-07-25 21:52:28 +0600

    Feature: Completed RAG Pipeline

---
+++ app/core.py
@@ -0,0 +1,22 @@
+from memory import memory
+from prompt_template import prompt_template
+from retrieval import retrieve_with_scores
+from clients.cohere_client import co
+from clients.llm_client import llm
+from langchain.schema import HumanMessage
+
+def rag_chat_with_memory(query: str) -> str:
+    memory.chat_memory.add_user_message(query)
+
+    bm25_results = retrieve_with_scores(query)
+    top_texts = [res["context"] for res in bm25_results[:10]]
+
+    rerank_res = co.rerank(query=query, documents=top_texts, top_n=3, return_documents=True)
+    final_texts = [res.document.text for res in rerank_res.results]
+
+    context = "\n".join(final_texts)
+    prompt = prompt_template.format(context=context, question=query)
+
+    answer = llm.invoke([HumanMessage(content=prompt)]).content.strip()
+    memory.chat_memory.add_ai_message(answer)
+    return answer
